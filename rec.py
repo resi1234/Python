7b0f5a5b47562a47fc2e76830ea8115877277c6b
Ubuntu <ubuntu@ip-172-31-5-1.ap-south-1.compute.internal>
2019-05-21 22:35:09 +0000

    rec socket

---
+++ rec.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import socket
+ip="0.0.0.0" # 1024 --65535 free range of port
+port=8888  #generate type of socket
+
+#generate protocol type of socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # udp docket created
+
+#to bind i can combine ip and port
+
+s.bind((ip,port))
+while True:
+    web=s.recvfrom(100)
+    print(web) 
+ #reply to sender
+    s.sendto("okay".encode)
+
